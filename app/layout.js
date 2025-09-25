e4c626bdf5e9a80d6a7644c45ad58aa337295c2e
bandajo094@gmail.com <joshua>
2025-09-25 13:22:56 -0700

    sshq

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon-new.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         {children}
